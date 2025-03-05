5202cd9d0cbfd73bf11611f880599002b4ccc8be
sakshibhardwaj9961 <sakshibhardwaj9961@gmail.com>
2025-03-05 15:26:56 +0530

    add bookDto

--- DTO/BookDto.cs
+++ DTO/BookDto.cs
@@ -1,20 +1,45 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 
 namespace DTO
 {
-    public class BookDto
+    public class BookRequestDto
     {
+        [Required(ErrorMessage = "Book Title is Necessary!")]
+
+        public string Title { get; set; }
+        [Required(ErrorMessage = "Enter Author Name!")]
+        public string Author { get; set; }
+        [Required(ErrorMessage = "Enter Publisher Name!")]
+        public string Publisher { get; set; }
+        public string Genere { get; set; }
+        [Required(ErrorMessage = "ISBN Number is Necessary!")]
+        [MinLength(13, ErrorMessage = "ISBN code must be of 13 Digits")]
+        public string ISBN { get; set; }
+        [Required(ErrorMessage = "Enetr Year in which Book Published!")]
+        public int YearPublished { get; set; }
+        public int CopiesAvailable { get; set; }
+        [Required(ErrorMessage = "Book Shelf Number is Necessary!")]
+        public int BookShelfNumber { get; set; }
+        public IFormFile Picture { get; set; }
+    }
+    public class BookResponseDto
+    {
+        public Guid Id { get; set; }
         public string Title { get; set; }
         public string Author { get; set; }
         public string Publisher { get; set; }
-        public string Genere { get; set; }
+        public string Genre { get; set; }
         public string ISBN { get; set; }
         public int YearPublished { get; set; }
         public int CopiesAvailable { get; set; }
         public int BookShelfNumber { get; set; }
+        public string Picture { get; set; }
+
     }
 }
